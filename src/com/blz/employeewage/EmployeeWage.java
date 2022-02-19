713f2ae5c5de55d7d8931475a44f8c7215519fe6
Priya <Priyamadiwalar0007@gmail.com>
2022-02-19 21:14:46 +0530

    Added: Attendance

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,7 +1,16 @@
 package com.blz.employeewage;
-
-public class EmployeeWage {
+ import java.util.*;
+  public class EmployeeWage {
 	    public static void main(String[] args) {
-		   System.out.println("Welcome EmployeeWage");	
+		   
+	    	int IS_FULL_TIME = 1;          // constants value
+	    	//Computation
+	    	Random random = new Random();
+	    	int empcheck = random.nextInt(2);
+	    	
+	        if (empcheck == IS_FULL_TIME)
+	    	   System.out.println("Employee is Present");
+	       else
+	    	   System.out.println("Employee is Abcent");
 	}
 }
