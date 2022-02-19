d606283c8078f28c8af371f39431444f57340332
Priya <Priyamadiwalar0007@gmail.com>
2022-02-19 21:42:05 +0530

    Added: Add_PartTime

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -4,15 +4,18 @@ package com.blz.employeewage;
 	    public static void main(String[] args) {
 		   
 	    	int IS_FULL_TIME = 1;                   // constants value
+	    	int IS_PART_TIME = 2;
 	    	int EMP_RATE_PER_HOUR =20;              // constants value 
 	    	int empHRs = 0;                           // Variables
   	    	int empWage = 0;                          // Variables
   	    	Random random = new Random();
-	    	int empcheck = random.nextInt(2);       //Computation 
+	    	int empcheck = random.nextInt(3);       //Computation 
 	    	
 	        if (empcheck == IS_FULL_TIME)
 	                 empHRs = 8;
-	       else 
+	       else if (empcheck == IS_PART_TIME)
+	    	         empHRs = 4;
+	       else
 	    	         empHRs = 0;
 	                 
 	            empWage= empHRs*EMP_RATE_PER_HOUR;
